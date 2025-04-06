0976bd2284dde6707fe066e799ad9e27ab6cb48b
Cleberton7 <cleberton622@gmail.com>
2025-04-06 19:37:57 -0300

    Conectando API google Maps

--- frontend/src/components/Content.jsx
+++ frontend/src/components/Content.jsx
@@ -1,24 +1,58 @@
 import React from 'react';
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
 import "../pages/styles/content.css";
 
+const containerStyle = {
+  width: '100%',
+  height: '400px',
+};
 
+const center = {
+  lat: -3.7657, // Tucuruí-PA
+  lng: -49.6725,
+};
 
 const Content = () => {
+  const apiKey = import.meta.env.VITE_GOOGLE_MAPS_KEY;
+  console.log("Minha API Key:", import.meta.env.VITE_GOOGLE_MAPS_KEY);
+
   return (
     <div className='content'> 
-        <div className='containerRanked'> 
-          <div id='textRanked'> Ranking das empresas </div>
-          <div id='rankedEmpresa'> Ranked empresas </div>
+      <div className='containerRanked'> 
+        <div id='textRanked'> Ranking das empresas </div>
+        <div id='rankedEmpresa'> Ranked empresas </div>
+      </div>
+
+      <div className='containerGraphic'> 
+        <div id='textGraph'> Gráfico de coletas </div>
+        <div id='graphic'> {/* gráfico aqui depois */} </div>
+      </div>
+      
+      <div className='containerMaps'> 
+        <div id='textLoc'> Localização/empresas </div>
+        <div id='maps'>
+          {apiKey ? (
+            
+              <GoogleMap
+                mapContainerStyle={containerStyle}
+                center={center}
+                zoom={13}
+                options={{
+                  mapId: '417aa5d2a794f56',
+                }}
+              >
+                {/* Aqui vai o marcador avançado */}
+                <gmp-advanced-marker
+                  position={`${center.lat},${center.lng}`}
+                  title="Centro de Tucuruí"
+                />
+              </GoogleMap>
+         
+          ) : (
+            <p>Erro: API Key não encontrada</p>
+          )}
         </div>
-        <div className='containerGraphic'> 
-          <div id='textGraph'> Grafico de coletas </div>
-          <div id='graphic'> Grafico </div>
-        </div>
-        
-        <div className='containerMaps'> 
-          <div id='textLoc'> Localização/empresas </div>
-          <div id='maps'> Maps</div>
-        </div> 
+      </div> 
     </div>
   );
 };
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import "./styles/Home.css";
-import Content from '../components/content.jsx'
-import Section from '../components/section.jsx';
+import Content from '../components/Content.jsx';
+import Section from '../components/Section.jsx';
+import { LoadScript } from "@react-google-maps/api";
 
 const Home = () => {
   return (
-    <div className='layouHome'>
-     
-      <div id='content'>
-        <Content/>
-      </div>
-
+    <LoadScript googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS_KEY}>
+      <div className='layouHome'>
+        <div id='content'>
+          <Content />
+        </div>
         <div id='section'>
-        <Section/>
+          <Section />
+        </div>
       </div>
-    </div>
+    </LoadScript>
   );
 };
 
