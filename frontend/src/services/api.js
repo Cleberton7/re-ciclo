ff8d867d042bf8cadf4cc4b8c88cf76b262d4237
Cleberton7 <cleberton622@gmail.com>
2025-06-18 20:20:42 -0300

    teste vite.config

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import * as jwtDecode from 'jwt-decode';
 
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL|| 'http://localhost:5000/api',
   timeout: 30000, // Aumente o timeout para 30 segundos
 });
 
